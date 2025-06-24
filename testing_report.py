fecbe255faf2cb46f0882dc901ce7bbd4164e355
Elvis Sierra <elvissierra3@gmail.com>
2025-06-24 13:45:31 -0500

    clean dirs

--- testing_report.py
+++ testing_report.py
@@ -14,7 +14,7 @@ EX. Cat Root Node,ModernCat-Name,              ,   yes    ,   yes    ,    .     
     
 """
 
-def find_latest_report(directory='.'):
+def find_latest_report(directory='csv_files/'):
     excluded_file = {"report_config.csv", "Analytics_Report.csv"}
     csv_files = glob.glob(os.path.join(directory, "*.csv"))
     csv_files = [f for f in csv_files if os.path.basename(f) not in excluded_file]
@@ -36,7 +36,7 @@ def write_custom_report(output_path, section_data):
             writer.writerows(section)
             writer.writerow([])
 
-def generate_dynamic_report(report_df, config_df, output_path="Analytics_Report.csv"):
+def generate_dynamic_report(report_df, config_df, output_path="csv_files/Analytics_Report.csv"):
     total_rows = len(report_df)
     section_blocks = []
 
@@ -98,7 +98,7 @@ if __name__ == "__main__":
         raise FileNotFoundError("No valid report CSV found.")
     print(f"📄 Using latest report: {latest_report}")
 
-    config_path = "report_config.csv"
+    config_path = "csv_files/report_config.csv"
     config_df = load_config_file(config_path)
     report_df = pd.read_csv(latest_report)
 
