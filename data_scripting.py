3c8fd1850cc7f056d4c5cd77414b231faa42fb25
Elvis Sierra <elvissierra3@gmail.com>
2025-07-14 13:21:39 -0500

    file printing

---
+++ data_scripting.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+files = ("file1", "file2", "file3", "file4", "file5")
+file_count = ("1st", "2nd", "3rd", "proceeding")
+
+for i, f in enumerate(files):
+    if i == 0:
+        print(f"This is the {file_count[0]}:", f)
+    elif i == 1:
+        print(f"This is the {file_count[1]}:", f)
+    elif i == 2:
+        print(f"This is the {file_count[2]}:", f)
+    else:
+        print(f"These are the {file_count[3]}:", f)
